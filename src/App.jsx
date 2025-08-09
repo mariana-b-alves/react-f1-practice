7bb007c9951193a022c869f20281b01a8981aaa7
Mariana Alves <m.bourdain.alves>
2025-08-09 15:53:19 +0100

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePageIndex from './components/HomePageIndex';
 import AboutPage from './components/AboutPage';
 import RacersPage from './components/RacersPage';
 
-import '../public/styles/index.css'
+import '../public/styles/index.css';
 import './App.css';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/react-f1-practice/" element={<HomePageIndex />} />
+        <Route path="/" element={<HomePageIndex />} />
         <Route path="/about" element={<AboutPage />} />
-        <Route path="/racers" element={<RacersPage />} /> 
+        <Route path="/racers" element={<RacersPage />} />
       </Routes>
     </Router>
   );
